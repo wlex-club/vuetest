a4d495b27873789ed35aeef4ed4829e1b1480299
williamWang <251887063@qq.com>
2018-07-26 23:40:53 +0800

    ceshi

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by william on 2018/7/26.
+ */
+
+import React, {Component} from 'react';
+
+class CaseMap extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="">
+            </div>
+        )
+    }
+}
+
+export default CaseMap;
+
